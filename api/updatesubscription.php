fafbe28ac9e1e7eb44cc9c00ebfa8c6836d79fc5
Saumini.Navaratnam <Saumini.Navaratnam@vocanic.com>
2017-03-09 14:18:52 +0800

    Update plan

---
+++ api/updatesubscription.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once('init.php');
+
+$subscriptionId	= $_POST['subscription'];
+$newPlan		= $_POST['plan'];
+
+$subscription = \Stripe\Subscription::retrieve($subscriptionId);
+$subscription->plan	= $newPlan;
+$subscription->save();
+
+echo $subscription->__toJSON();
