497bdfeb186882db526b845eb519c283e73ece9e
Saumini.Navaratnam <Saumini.Navaratnam@vocanic.com>
2017-03-16 11:29:26 +0800

    Improved listing plans

--- README.md
+++ README.md
@@ -4,6 +4,11 @@
 
 1. In Vsocial end have to store the stripe customer id
 
+### Things I tried
+
+1. Listing plans - Check following link https://dev1.vocanic.net/stripe/
+
+
 ### Things to go thorugh
 
 ###### 07/03/2017
--- api/listplans.php
+++ api/listplans.php
@@ -2,5 +2,26 @@
 
 require_once('init.php');
 
-$plans = \Stripe\Plan::all();
+// 1 - Using Stripe PHP library
+try {
+	
+	$plans = \Stripe\Plan::all();
+	
+} catch(Exception $e) {
+	echo json_encode(array('error' => array('message' => $e->getMessage())));
+	return;
+}
+
 echo $plans->__toJSON();
+
+// // 2 - Using curl
+// $ch = curl_init();
+
+// curl_setopt($ch, CURLOPT_URL, 'https://api.stripe.com/v1/plans');
+// curl_setopt($ch, CURLOPT_USERPWD, "sk_test_SHbu5VHieCiBSe7jdwwgZJ0H:");
+// curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+// $output = curl_exec($ch);
+
+// curl_close($ch);
+// echo $output;
